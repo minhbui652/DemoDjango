9c17c521b2a2996a8209ebf45773d8c283f5aa98
Minh Bui <minhbui6502@gmail.com>
2025-04-11 09:43:36 +0700

    add .gitignore, .evn hide secret_key, databaseName, databaseUser, databasePassword

--- TaskManagement/settings.py
+++ TaskManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gq$ur%p&n!=n+akh8mqaf@aq032vuq956(@07sp0q(ys0orx&%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,9 +88,9 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'DemoDjango',
-        'USER': 'sa',
-        'PASSWORD': '',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         # 'PORT': '',
         'OPTIONS': {
